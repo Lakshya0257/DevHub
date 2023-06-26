d5999fa6143d5c7cb74a47d4e2c6c4c7b5027b31
Lakshya0257 <lakshyabhati24@gmail.com>
2023-06-27 00:33:42 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/navBar/nav-bar/nav-bar.component';
 import { ChattingComponent } from './views/chatting/chatting/chatting.component';
+import { ContactComponent } from './components/chatting/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    ChattingComponent
+    ChattingComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
