24159991ab062a96a3c00c19515d40948acfc005
Lakshya0257 <lakshyabhati24@gmail.com>
2023-06-26 17:36:10 +0530

    first push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/navBar/nav-bar/nav-bar.component';
+import { ChattingComponent } from './views/chatting/chatting/chatting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ChattingComponent
   ],
   imports: [
     BrowserModule,
